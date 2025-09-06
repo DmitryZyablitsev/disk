7fbbbcd2e3761c7a3b08621b89adad6d94a2ed11
DmitryZyablitsev <118909453+DmitryZyablitsev@users.noreply.github.com>
2025-09-06 13:31:57 +0500

    Add cd iteration 2 (#6)
    * add Anseble, Ansible.mk
    
    * Update Ansible.mk and inventory1.ini
    
    * Update docker-compose.production.yml
    
    * Add deploy.yml
    
    Add nginx in reposetory
    
    fix path deploy.yml
    
    fix path deploy.yml v2
    
    fix path docker-compose
    
    nginx вынесен в ansible
    
    deploy add sudo

---
+++ ansible/Ansible.mk
@@ -0,0 +1,24 @@
+# Очистите старый ключ
+# ssh-keygen -f ~/.ssh/known_hosts -R '45.138.74.25'
+
+# Добавьте новый ключ
+# ssh-keyscan -H 45.138.74.25 >> ~/.ssh/known_hosts
+hardening-step-1:
+	ssh-keygen -f ~/.ssh/known_hosts -R '95.81.121.50'
+	ssh-keyscan -H 95.81.121.50 >> ~/.ssh/known_hosts
+	ansible-playbook ansible/hardening_step_1.yml -i ansible/inventory1.ini
+
+hardening-step-2:
+	ansible-playbook ansible/hardening_step_2.yml -i ansible/inventory2.ini
+
+docker-setup:
+	ansible-playbook ansible/docker_setup.yml -i ansible/inventory3.ini
+
+install-nginx:
+	ansible-playbook ansible/install_nginx_ssl.yml -i ansible/inventory3.ini
+
+ansible-start:
+	$(MAKE) hardening-step-1
+	$(MAKE) hardening-step-2
+	$(MAKE) docker-setup
+	$(MAKE) install-nginx
